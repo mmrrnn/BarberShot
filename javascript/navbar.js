8f94316c7b5d053698e462001658a5009cc769b2
Marcin Papież <mmpapiez@gmail.com>
2020-03-10 17:50:40 +0100

    v4.0

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,16 +1,16 @@
-$(document).ready(function () {
-    $(window).scroll(function () {
-        if ($(window).scrollTop() === 0) {
-            $('#navbar').addClass('navbar-dark');
-            $('#navbar').removeClass('navbar-light');
-            $('#dropdown-menu').addClass('dropdown-dark');
-            $('#dropdown-menu').removeClass('dropdown-light');
-        } else {
-            $('#navbar').addClass('navbar-light');
-            $('#navbar').removeClass('navbar-dark');
-            $('#dropdown-menu').addClass('dropdown-light');
-            $('#dropdown-menu').removeClass('dropdown-dark');
-        }
-    });
-});
+// $(document).ready(function () {
+//     $(window).scroll(function () {
+//         if ($(window).scrollTop() === 0) {
+//             $('#navbar').addClass('navbar-dark');
+//             $('#navbar').removeClass('navbar-light');
+//             $('#dropdown-menu').addClass('dropdown-dark');
+//             $('#dropdown-menu').removeClass('dropdown-light');
+//         } else {
+//             $('#navbar').addClass('navbar-light');
+//             $('#navbar').removeClass('navbar-dark');
+//             $('#dropdown-menu').addClass('dropdown-light');
+//             $('#dropdown-menu').removeClass('dropdown-dark');
+//         }
+//     });
+// });
 
